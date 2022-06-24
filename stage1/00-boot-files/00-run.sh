e41b2c301cbf850145f97da7e42da8150366864c
Mohammad Taha Bin Firoz <mohammdh@live.com>
2022-06-25 02:57:33 +0500

    add a userconf for bipass username wizard

--- stage1/00-boot-files/00-run.sh
+++ stage1/00-boot-files/00-run.sh
@@ -2,3 +2,10 @@
 
 install -m 644 files/cmdline.txt "${ROOTFS_DIR}/boot/"
 install -m 644 files/config.txt "${ROOTFS_DIR}/boot/"
+
+# Add userconf file for first boot
+on_chroot << EOF
+if [ -n "${FIRST_USER_PASS}" ]; then
+    echo "$FIRST_USER_NAME":$(openssl passwd -6 "$FIRST_PASSWORD") >> "${ROOTFS_DIR}/boot/userconf.txt"
+fi
+EOF
